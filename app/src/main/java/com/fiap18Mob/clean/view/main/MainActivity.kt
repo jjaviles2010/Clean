0369f01cc63d8d778e62a5210f6caa195c0c19ab
Vilson Passos <vilson.passosss@gmail.com>
2019-11-12 01:58:17 -0300

    Criando a tela de home do cliente

--- app/src/main/java/com/fiap18Mob/clean/view/main/MainActivity.kt
+++ app/src/main/java/com/fiap18Mob/clean/view/main/MainActivity.kt
@@ -6,19 +6,12 @@ import android.os.Bundle
 import android.view.View
 import com.fiap18Mob.clean.R
 import com.fiap18Mob.clean.view.about.AboutActivity
-import com.fiap18Mob.clean.view.signup.SignUpActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        btnTest.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
-        }
-
+        setContentView(R.layout.activity_home)
     }
 
 
