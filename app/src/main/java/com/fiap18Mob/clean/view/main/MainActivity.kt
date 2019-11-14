b10a841a4f9f111d33a5345a637c8558c9e43ef9
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2019-11-14 01:24:59 -0300

    Incluindo campos na tela de Detalhes da Diarista.

--- app/src/main/java/com/fiap18Mob/clean/view/main/MainActivity.kt
+++ app/src/main/java/com/fiap18Mob/clean/view/main/MainActivity.kt
@@ -6,12 +6,19 @@ import android.os.Bundle
 import android.view.View
 import com.fiap18Mob.clean.R
 import com.fiap18Mob.clean.view.about.AboutActivity
+import com.fiap18Mob.clean.view.cleanerDetail.CleanerDetailActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btSchedules.setOnClickListener {
+            val intent = Intent(this@MainActivity, CleanerDetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
