22b0b9fb37adc2ea211722e6afea9a7a39e78fdd
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2019-11-15 23:46:52 -0300

    Merge conflicts

---
+++ app/src/main/java/com/fiap18Mob/clean/view/usersList/MainAdapter.kt
@@ -0,0 +1,41 @@
+package com.fiap18Mob.clean.view.usersList
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.fiap18Mob.clean.R
+import com.fiap18Mob.clean.repository.UserRepositoryRemote
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.user_row.view.*
+
+class MainAdapter(): RecyclerView.Adapter<CustomViewHolder>() {
+
+    val userRemoteRep = UserRepositoryRemote(FirebaseAuth.getInstance(), FirebaseDatabase.getInstance())
+    val infoClients = userRemoteRep.getUsersByProfile("CLEANER", onComplete = { }, onError = { })
+
+    // number of items
+    override fun getItemCount(): Int {
+        return infoClients.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.user_row, parent, false)
+
+        return CustomViewHolder(cellForRow)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        val userInfo = infoClients.get(position)
+        holder.view.textViewName.text = userInfo.nome
+    }
+
+}
+
+
+
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+}
