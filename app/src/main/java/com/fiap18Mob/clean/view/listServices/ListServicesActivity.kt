ed1decda2494b24201b2022410eeee3c79018d00
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2020-01-10 10:21:40 -0300

    Criando icone de delete no swipe da lista de serviços agendados.

--- app/src/main/java/com/fiap18Mob/clean/view/listServices/ListServicesActivity.kt
+++ app/src/main/java/com/fiap18Mob/clean/view/listServices/ListServicesActivity.kt
@@ -1,8 +1,12 @@
 package com.fiap18Mob.clean.view.listServices
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.*
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +19,8 @@ import kotlinx.android.synthetic.main.activity_list_services.*
 import kotlinx.android.synthetic.main.include_loading.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
+import android.graphics.drawable.Drawable
+
 
 class ListServicesActivity : BaseActivity() {
 
@@ -63,7 +69,7 @@ class ListServicesActivity : BaseActivity() {
     }
 
     private fun setRecyclerViewItemTouchListener() {
-        val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
             override fun onMove(
                 recyclerView: RecyclerView,
                 viewHolder: RecyclerView.ViewHolder,
@@ -75,12 +81,59 @@ class ListServicesActivity : BaseActivity() {
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
 
-                if (direction == ItemTouchHelper.LEFT || direction == ItemTouchHelper.RIGHT) {
-                    listServicesViewModel.deleteCleaningServices(listServicesViewModel.services.value!![position])
-                    listServicesViewModel.services.value?.removeAt(position)
-                    rvListServices.adapter!!.notifyItemRemoved(position)
-                }
+                listServicesViewModel.deleteCleaningServices(listServicesViewModel.services.value!![position])
+                listServicesViewModel.services.value?.removeAt(position)
+                rvListServices.adapter!!.notifyItemRemoved(position)
+
             }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                val p = Paint()
+
+                val icon: Drawable
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+
+                    val itemView = viewHolder.itemView
+                    val height = itemView.bottom - itemView.top
+                    val width = height / 3
+
+                    if (dX > 0) {
+                        p.color = Color.parseColor("#D32F2F")
+                        val background =
+                            RectF(itemView.left.toFloat(), itemView.top.toFloat(), dX, itemView.bottom.toFloat())
+                        c.drawRect(background, p)
+                        icon = ContextCompat.getDrawable(this@ListServicesActivity, R.drawable.ic_delete_black_24dp)!!;
+                        val icon_dest = Rect(
+                            itemView.left + width,
+                            itemView.top + width,
+                            itemView.left + 2 * width,
+                            itemView.bottom - width
+                        )
+                        icon.setBounds(icon_dest)
+                        icon.draw(c)
+                    }
+
+                }
+
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+            }
+
 
         }
 
